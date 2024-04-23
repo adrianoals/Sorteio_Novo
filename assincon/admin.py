80e3ef5d34616229f528272b64b546c2c39094f6
Adriano Lima <dri.limasantos@gmail.com>
2024-04-23 15:50:18 -0300

    up

--- assincon/admin.py
+++ assincon/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from assincon.models import ListaPresenca, ListaSindicos, Sorteio
-
-class ListaPresencaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'brinde')
-    list_display_links = ('id', 'nome', 'brinde')	
-
+from assincon.models import ListaSindicos, Sorteio
 
 class ListaSindicosAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome')
@@ -14,7 +9,6 @@ class SorteioAdmin(admin.ModelAdmin):
     list_display = ('id', 'sindico', 'sorteado')
     list_display_links = ('id', 'sindico', 'sorteado')	
 
-admin.site.register(ListaPresenca, ListaPresencaAdmin)
 admin.site.register(ListaSindicos, ListaSindicosAdmin)
 admin.site.register(Sorteio, SorteioAdmin)
 
--- assincon/models.py
+++ assincon/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-class ListaPresenca(models.Model):
-    nome = models.CharField(max_length=150,)
-    brinde = models.BooleanField(default=False)
-    def __str__(self):
-        return f'{self.nome}'
-
 class ListaSindicos(models.Model):
     nome = models.CharField(max_length=150,)
     def __str__(self):
@@ -17,3 +11,6 @@ class Sorteio(models.Model):
 
     def __str__(self):
         return f"{self.sindico.nome} - {'Sorteado' if self.sorteado else 'Não Sorteado'}"
+    
+
+    
