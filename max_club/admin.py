7282dc46575a93daced1f59528dda51cd4d311e7
dri.limasantos@gmail.com <dri.limasantos@gmail.com>
2024-07-02 20:12:47 -0300

    up

--- max_club/admin.py
+++ max_club/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from max_club.models import Apartamento, Sorteio, Vaga
 
-# Register your models here.
+
+class ApartamentoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'numero_apartamento')
+    list_display_links = ('id', 'numero_apartamento')
+    
+class VagaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vaga')
+    list_display_links = ('id', 'vaga')
+
+
+class SorteioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'apartamento', 'vaga')
+    list_display_links = ('id', 'apartamento', 'vaga')
+
+admin.site.register(Apartamento, ApartamentoAdmin)
+admin.site.register(Vaga, VagaAdmin)
+admin.site.register(Sorteio, SorteioAdmin)
--- max_club/views.py
+++ max_club/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Apartamento, Vaga, Sorteio
+from django.utils import timezone
+import random
+from django.contrib.admin.views.decorators import staff_member_required
 
-# Create your views here.
+# Excel
+from openpyxl import load_workbook
+from django.http import HttpResponse
+from django.utils import timezone
+from django.contrib import messages
+
+
+@staff_member_required
+def max_club(request):
+    if request.method == 'POST':
+        # Limpar registros anteriores de sorteio
+        Sorteio.objects.all().delete()
+        
+        # Obter todos os apartamentos e grupos de vagas
+        apartamentos = list(Apartamento.objects.all())
+        vagas = list(Vaga.objects.all())
+
+        # Certifique-se de que existem vagas suficientes para todos os apartamentos
+        if len(vagas) >= len(apartamentos):
+            random.shuffle(vagas)
+
+            for apartamento in apartamentos:
+                vaga_selecionada = vagas.pop()
+                Sorteio.objects.create(
+                    apartamento=apartamento, 
+                    vaga=vaga_selecionada
+                )
+        else:
+           
+            pass
+        
+        # Armazenar informações do sorteio na sessão
+        request.session['sorteio_iniciado_nc'] = True
+        request.session['horario_conclusao_nc'] = timezone.localtime().strftime("%d/%m/%Y às %Hh e %Mmin e %Ss")
+
+        return redirect('max_club')
+    
+    else:
+        sorteio_iniciado_nc = request.session.get('sorteio_iniciado_nc', False)
+        resultados_sorteio_nc = Sorteio.objects.select_related('apartamento', 'vaga').order_by('apartamento__id').all()
+        vagas_atribuidas_nc = resultados_sorteio_nc.exists()  # Verificar se existem resultados
+
+        return render(request, 'max_club/max_club.html', {
+            'resultados_sorteio_nc': resultados_sorteio_nc,
+            'vagas_atribuidas_nc': vagas_atribuidas_nc,
+            'sorteio_iniciado_nc': sorteio_iniciado_nc,
+            'horario_conclusao_nc': request.session.get('horario_conclusao_nc', '')
+        })
