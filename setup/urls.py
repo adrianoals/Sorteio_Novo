17ed350c11aa577373af8c0720294a220d7932a5
dri.limasantos@gmail.com <dri.limasantos@gmail.com>
2024-10-08 12:40:34 -0300

    up

--- setup/urls.py
+++ setup/urls.py
@@ -3,13 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', include('sorteio_novo.urls')), 
-    path('', include('assincon.urls')), 
-    path('', include('porcelana.urls')), 
-    path('', include('lyon.urls')), 
-    path('', include('helbor.urls')), 
-    path('', include('nova_guarulhos.urls')), 
-    path('', include('max_club.urls')), 
     path('', include('tres_coelhos.urls')), 
 
 ]
